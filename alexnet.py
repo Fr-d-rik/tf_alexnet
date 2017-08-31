e4fb832f78305901e9befad1730a9b95f7dc0eb5
Fr-d-rik <frederikharder@gmail.com>
2017-08-31 18:38:49 +0200

    started enabling partial build

--- alexnet.py
+++ alexnet.py
@@ -29,6 +29,15 @@ class AlexNet:
 
         self.data_dict = np.load(weights_path, encoding='latin1').item()
         self.imagenet_mean = np.mean([123.68, 116.779, 103.939])  # imagenet mean (channel-wise to global)
+        self.names = ['input', 'rgb_scaled', 'bgr_normed',
+                      'conv1/lin', 'conv1/relu', 'lrn1', 'pool1',
+                      'conv2/lin', 'conv2/relu', 'lrn2', 'pool2',
+                      'conv3/lin', 'conv3/relu',
+                      'conv4/lin', 'conv4/relu',
+                      'conv5/lin', 'conv5/relu', 'pool5',
+                      'fc6/lin', 'fc6/relu',
+                      'fc7/lin', 'fc7/relu',
+                      'fc8/lin', 'fc8/relu', 'softmax']
 
     def build(self, rgb, rescale=255.0):
 
@@ -100,3 +109,76 @@ class AlexNet:
             fc = tf.nn.bias_add(tf.matmul(in_tensor, weights), biases, name='lin')
             relu = tf.nn.relu(fc, name='relu')
             return relu, fc
+
+    def build_partial(self, in_tensor, input_name, rescale=255.0):
+
+        if 'lin' in input_name:
+            in_tensor = tf.nn.relu(in_tensor)
+            input_name.replace('lin', 'relu')
+
+        assert input_name in self.names
+        names_to_build = [n for n in self.names if 'lin' not in n]
+        print(len(names_to_build))
+        start = names_to_build.index(input_name)
+        names_to_build = names_to_build[start:]
+        print(names_to_build)
+
+        build_ops = []
+        build_ops.append(lambda x: tf.multiply(x, rescale, name='rgb_scaled'))
+
+        def rgb2bgr(x):
+            rgb_normed = x - self.imagenet_mean
+            red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_normed)
+            return tf.concat(axis=3, values=[blue, green, red], name='bgr_normed')
+
+        build_ops.append(rgb2bgr)
+
+        # conv1
+        build_ops.append(lambda x: self.convolution(x, s_h=4, s_w=4, group=1, name='conv1'))
+        build_ops.append(lambda x: tf.nn.local_response_normalization(x, depth_radius=2, alpha=2e-05,
+                                                                      beta=0.75, bias=1.0, name='lrn1'))
+        build_ops.append(lambda x: tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                                                  padding='VALID', name='pool1'))
+
+        # conv2
+        build_ops.append(lambda x: self.convolution(x, s_h=1, s_w=1, group=2, name='conv2'))
+        build_ops.append(lambda x: tf.nn.local_response_normalization(x, depth_radius=2, alpha=2e-05,
+                                                                      beta=0.75, bias=1.0, name='lrn2'))
+        build_ops.append(lambda x: tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                                                  padding='VALID', name='pool2'))
+
+        # conv3
+        build_ops.append(lambda x: self.convolution(x, s_h=1, s_w=1, group=1, name='conv3'))
+
+        # conv4
+        build_ops.append(lambda x: self.convolution(x, s_h=1, s_w=1, group=2, name='conv4'))
+
+        # conv5
+        build_ops.append(lambda x: self.convolution(x, s_h=1, s_w=1, group=2, name='conv5'))
+        build_ops.append(lambda x: tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                                                  padding='VALID', name='pool5'))
+
+        # flatten
+        # noinspection PyTypeChecker
+
+        # fc6
+        build_ops.append(lambda x: self.fc_layer(in_tensor=tf.reshape(x, [-1, int(np.prod(x.get_shape()[1:]))],
+                                                                      name='pool5_flat'), name='fc6'))
+        build_ops.append(lambda x: self.fc_layer(in_tensor=x, name='fc7')[0])
+        build_ops.append(lambda x: self.fc_layer(in_tensor=x, name='fc8')[0])
+
+        # prob
+        build_ops.append(lambda x: tf.nn.softmax(x))
+
+        print(len(build_ops))
+        print(len(self.names))
+        print(len(names_to_build))
+
+        build_ops = build_ops[-len(names_to_build):]
+        print(len(build_ops))
+
+        temp_tensor = in_tensor
+        for op in build_ops:
+            temp_tensor = op(temp_tensor)
+
+        self.data_dict = None
